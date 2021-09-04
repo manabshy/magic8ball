e5a74671be2042d3b2adaa26573d7da5766c732c
MJ <manabshy@gmail.com>
2021-09-04 15:26:18 +0100

    Add Javascript

--- js/script.js
+++ js/script.js
@@ -1,37 +1,38 @@
-/**
- * Here are all the possible fortunes we can get
- * from the Magic 8-Ball!
- */
- let messages = ["It is certain", "It is decidedly so", "Without a doubt", "Yes – definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Don’t count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful", "Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again"];
+console.log('Hello World !!!');
 
- // build a function that
- // gets a random message.
- // I'll get that nth index inside of the
- // array, and then i'll return it!
+let myVar = 10;
 
- function getEightballMessage() {
-   // To get the message,
-   // Pick a random number between 1 and however
-   // long that array is.
-   let messagesSize = messages.length;
-   let randomNumber = Math.floor(Math.random() * messagesSize);
-   return messages[randomNumber];
- }
+console.log('myVar:', myVar);
 
- // Somehow, we need to do the following:
- function changeMessage() {
-   // Somehow it should clear the eight ball message
-   let eightEl = document.getElementById('eight');
-   eightEl.textContent = '';
+var myArray = ['Apple', 'Banana'];
 
-   // We want to get our getEightballMessage...
-   let answerEl = document.getElementById('answer');
+var myButtons = ['Submit', 'Chat', 'Learn More'];
 
-   // ...and put the message into the <p id="answer">.
-   answerEl.textContent = getEightballMessage();
- }
+myButtons.forEach((button, index) => console.info('Button' + index + ':' + button));
 
- // We want a way to call changeMessage()
- // when we click on the shake button!
- let buttonEl = document.getElementById('button');
- buttonEl.addEventListener('click', changeMessage);
+
+function chat() {
+
+  alert('Please wait for your invitation to the chatroom');
+}
+
+function submitme() {
+
+  confirm('Do you wish to submit ?');
+  window.location.href = 'https://bbc.co.uk';
+}
+
+
+mychatbutton = document.getElementById('chat');
+
+console.log('Id of Chat has:', mychatbutton);
+
+
+mychatbutton.addEventListener("click", chat);
+
+
+mysubmitbutton = document.getElementById('submit');
+
+mysubmitbutton.addEventListener("click", submitme);
+
+
